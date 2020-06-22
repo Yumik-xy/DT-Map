0679e3eb402dbc8f8418cff3a7abfa989e9d9394
宇海 <44742784+Yumik-xy@users.noreply.github.com>
2020-06-22 18:59:56 +0800

    提交验证

--- DTMapdjango/api/views.py
+++ DTMapdjango/api/views.py
@@ -1,3 +1,6 @@
+import time
+import base64
+
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,9 +11,15 @@ from rest_framework.exceptions import ValidationError
 
 class LoginView(APIView):
     def post(self, request, *args, **kwargs):
-        print(request.data)
-        return Response({'status': True, 'code': '1'})
-
+        phone = request.data.get('phone')
+        encoder = request.data.get('encoder')
+        verification_code = request.data.get('Verification_Code')
+        decoder = base64.b64decode(encoder).decode('utf-8')
+        str=decoder.split('-')
+        stamp = time.time()
+        if phone == str[2] & stamp-float(str[0])<=90 & :
+            return Response({'status': True, 'code': '1'})
+        return Response({'status': False, 'message': '验证码错误或失效！'})
 
 # 对手机号进行格式校验的部分
 def phone_validator(value):
@@ -38,7 +47,7 @@ class GetCodeView(APIView):
         # 2.对手机号进行校验
         ser = GetCOdeSerializers(data=request.query_params)
         if not ser.is_valid():
-            return Response({'status': False, 'code': '-1'})
+            return Response({'status': False, 'message': '手机号格式错误'})
         phone = ser.validated_data.get('phone')
         # 3.生成随机验证码
         import random as rd
@@ -46,11 +55,8 @@ class GetCodeView(APIView):
         # 4.发送验证码（有效期60s） [一般情况下购买服务发短信]
         # TODO 完成验证码的发送，考虑到初赛阶段，不发送，后端可见即可！
         # 5.生成时间戳base64加密
-        import time
-        import base64
         stamp = str(time.time())
         url = '{0}-{1}-{2}'.format(stamp, random_code, phone)
-        encoder = base64.b64encode(url.encode("utf-8"))
-        str_url = encoder.decode('utf-8')
+        encoder = base64.b64encode(url.encode("utf-8")).decode('utf-8')
         # 6.返回加密数据
-        return Response({'status': True, 'code': str_url})
+        return Response({'status': True, 'encoder': encoder})
