f00fca6f0b29c412436a27442cd3c1a73c131776
宇海 <44742784+Yumik-xy@users.noreply.github.com>
2020-06-24 03:10:06 +0800

    可以了 能读入数据库了

--- DTMapdjango/api/admin.py
+++ DTMapdjango/api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib.auth.models import User
+from api.models import dtmap
+
+
+class dtmapAdmin(admin.ModelAdmin):
+    search_fields = ('id', 'name', 'phone', 'time')  # 根据属性搜索
+    list_display = ('id', 'name', 'phone', 'time')  # 列表显示的属性
+    # list_filter = ('id',)  # 筛选
+    pass
+
+
+admin.site.register(dtmap, dtmapAdmin)
--- DTMapdjango/api/models.py
+++ DTMapdjango/api/models.py
@@ -1,8 +1,17 @@
 from django.db import models
-from __future__ import unicode_literals
+
 
 from django.db import models
 
+
 # Create your models here.
 class dtmap(models.Model):
-    
+    id = models.IntegerField(help_text='UID必须唯一！例如：00001234', verbose_name='个人UID', primary_key=True, unique=True)
+    name = models.TextField(max_length=20, verbose_name='姓名')
+    phone = models.CharField(max_length=11, verbose_name='手机号')
+    time = models.DateTimeField(verbose_name='注册时间', blank=True)
+
+    class Meta:
+        verbose_name_plural = '注册用户查询'
+        verbose_name = 'ID'
+        db_table = 'user'
